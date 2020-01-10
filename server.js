d27e5892b744e3a05515170ad5539a3509c2cbe3
AzoozMac <abdulaziz-003@hotmail.com>
2020-01-10 15:04:58 +0300

    inital setup

---
+++ server.js
@@ -0,0 +1,40 @@
+// Check if we are running in the dev or local
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Import Index Router
+const indexRouter = require('./routers/index');
+
+// Setting View Engine To Be EJS
+app.set('view engine', 'ejs');
+app.set('views', __dirname+'/views');
+app.set('layout', 'layouts/layout');
+
+// Middleware
+// using express-ejs-layouts
+app.use(expressLayouts);
+// Using body-parser
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+
+
+// Import mongoose to the application
+const mongoose = require('mongoose');
+// Define connection with local mongoDB 
+mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true });
+// Start connection with mongoDB
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('connected to mongoDB :) '));
+
+// Index Router
+app.use('/', indexRouter);
+
+
+app.listen(process.env.PORT || 3000, ()=> console.log('Server is running...'));
